299eb04882fb4fe5f9aaace7e5b5fe7194181340
BenJ1337 <99%secure;)!>
2021-04-06 18:26:08 +0200

    if not exists, create log directory

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import  logging, yaml
+import  logging, yaml, os
 from logging.config import dictConfig
 from IpTracker import IpTracker
 
-print("Start")
-with open("../config/logger.yml") as logger_config_file:
-    dictConfig(yaml.safe_load(logger_config_file))
-
-logger = logging.getLogger("iplogger")
-
 if "__main__" == __name__:
+    logDir = '../logs'
+    if not os.path.isdir(logDir):
+        os.mkdir(logDir)
+
+    filename = 'message.log'
+    if not os.path.isfile(os.path.join(logDir, filename)):
+        with open(os.path.join(logDir, filename), 'w') as fp:
+            pass
+    
+    with open("../config/logger.yml") as logger_config_file:
+        dictConfig(yaml.safe_load(logger_config_file))
+
+    logger = logging.getLogger("iplogger")
+
     ipTracker = IpTracker()
     ipTracker.trackIP()
-
