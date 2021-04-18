3cb48fb5da4a50ddae23107e0a0a7069d4cdd54e
BenJ1337 <99%secure;)!>
2021-04-18 12:38:56 +0200

    fix http header

--- src/IpTracker.py
+++ src/IpTracker.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests, json, logging, time, random
+import requests
+import json
+import logging
+import time
+import random
 from datetime import datetime
 from GoogleMailer import GoogleMailer
 
+
 class IpTracker:
     oldIp = ''
-    userAgents = []
-    sleepMultiplier = 60
     url = ''
+    logger = logging.getLogger("iplogger")
 
     def __init__(self):
-        logger = logging.getLogger("iplogger")
         with open("../headers/crawler-user-agents.json") as header_file:
-            self.userAgents = json.load(header_file)
-        
-        with open("../config/url.config") as url_file:
-            urlConfig = json.load(url_file)
-            self.url = urlConfig['url']
-            logger.debug(self.url)
+            self.userAgentAgencies = json.load(header_file)
+
+        with open("../config/settings.config") as settings_file:
+            settings = json.load(settings_file)
+            self.url = settings['url']
+            self.enableMailinfo = settings['enableMailinfo']
+            self.sleepDuration = settings['sleepDuration']
+            self.logger.debug(self.url)
+
+    def selectUserAgent(self):
+        userAgentAgency = random.choice(self.userAgentAgencies)
+        userAgents = userAgentAgency['instances']
+        if (len(userAgents) < 1):
+            return self.selectUserAgent()
+        else:
+            userAgent = random.choice(userAgents)
+            self.logger.debug('User-Agent: %s', userAgent)
+            return userAgent
+
+    def buildHTTPHeader(self):
+        header = {}
+        userAgent = self.selectUserAgent()
+        header['User-Agent'] = userAgent
+        self.logger.debug('Header: %s', header)
+        return header
 
     def trackIP(self):
-        logger = logging.getLogger("iplogger")
         try:
             while True:
-                userAgentAgency = random.choice(self.userAgents)
-                userAgent = random.choice(userAgentAgency['instances'])
-                logger.debug(userAgent)
-                resp = requests.get(self.url, userAgent)
+                resp = requests.get(
+                    self.url, self.buildHTTPHeader())
                 currTiemstamp = datetime.now()
                 status = resp.status_code
                 if status == 200:
@@ -38,22 +57,28 @@ class IpTracker:
                     if(currentIp != self.oldIp):
                         ipStatus = 'new'
                         googlemailer = GoogleMailer()
-                        googlemailer.mail('neue IP-Adresse: ' + currentIp, 'Der Anschluss hat eine neue IP-Adresse: ' + currentIp + '\n\nAlte IP-Adresse: ' + self.oldIp)
+                        if self.enableMailinfo:
+                            googlemailer.mail('neue IP-Adresse: ' + currentIp, 'Der Anschluss hat eine neue IP-Adresse: ' +
+                                              currentIp + '\n\nAlte IP-Adresse: ' + self.oldIp)
                         self.oldIp = currentIp
-                    logger.info("[%s] IP-Address > %s ", ipStatus, currentIp)
+                    self.logger.info("[%s] IP-Address > %s ",
+                                     ipStatus, currentIp)
                 else:
-                    logger.error("HTTP Response Status nicht ok: " + status)
+                    self.logger.error(
+                        "HTTP Response Status nicht ok: " + status)
                     self.ipMap[currTiemstamp] = {}
                     self.ipMap[currTiemstamp]['status'] = status
-                seconds2Sleep = random.randint(5,10) * self.sleepMultiplier
-                logger.info("Programm sleeps for %s seconds", seconds2Sleep)
-                time.sleep(seconds2Sleep)
+                self.logger.info(
+                    "Programm sleeps for %s seconds", self.sleepDuration)
+                time.sleep(self.sleepDuration)
         except KeyboardInterrupt:
-            logger.info("Schleife per STRG + C beendet")
+            self.logger.info("Schleife per STRG + C beendet")
         except Exception as ex:
-            logger.info("Exception while requesting ip address status: %s", getattr(ex, 'message', str(ex)))
+            self.logger.info("Exception while requesting ip address status: %s", getattr(
+                ex, 'message', str(ex)))
             googlemailer = GoogleMailer()
             msg = 'IP Tracker hat einen Fehlerfall und wurde beendet'
-            googlemailer.mail(msg, msg)
+            if self.enableMailinfo:
+                googlemailer.mail(msg, msg)
         finally:
-            logger.info("Programm beendet")
+            self.logger.info("Programm beendet")
